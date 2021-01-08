04ca4891403f53bce8afae9d2a093a4cf1428861
shihu <shihubu@bupt.edu.cn>
2021-01-09 01:06:54 +0800

    rebuild plugin

--- newip.lua
+++ newip.lua
@@ -91,63 +91,80 @@ do
 	end
 
     --协议名称为NewIP，在Packet Details窗格显示为NewIP
-    local p_NewIP = Proto("NewIP","NewIP")
-	-- 目的地址
-	local n_dest_address_TL = ProtoField.uint8("NewIP.dest_address_TL", "dest_address_TL", base.HEX)
+	local p_NewIP = Proto("NewIP","NewIP")
+	
+
+	-- 源地址 0x01
+	local n_source_address_TL = ProtoField.string("NewIP.source_address_TL", "source_address_TL", base.ASCII)
+	-- local n_source_address_level = ProtoField.string("NewIP.source_address_level", "source_address_level", base.NONE)
+	local n_source_address = ProtoField.string("NewIP.source_address", "source_address", base.ASCII)
+	
+	-- 目的地址 0x02
+	local n_dest_address_TL = ProtoField.string("NewIP.dest_address_TL", "dest_address_TL", base.ASCII)
 	-- local n_dest_address_level = ProtoField.string("NewIP.dest_address_level", "dest_address_level", base.NONE)
 	local n_dest_address = ProtoField.string("NewIP.dest_address", "dest_address", base.ASCII)
 	
-	-- 源地址
-	local n_source_address_TL = ProtoField.uint8("NewIP.source_address_TL", "source_address_TL", base.HEX)
-	-- local n_source_address_level = ProtoField.string("NewIP.source_address_level", "source_address_level", base.NONE)
-	local n_source_address = ProtoField.string("NewIP.source_address", "source_address", base.ASCII)
-
-	-- TTL
-	local n_TTL = ProtoField.uint8("NewIP.TTL", "n_TTL", base.DEC)
-	-- NextHeader
-	local n_protocol = ProtoField.uint8("NewIP.protocol", "n_protocol", base.HEX)
-	-- header_length
-	local n_header_length = ProtoField.uint8("NewIP.header_length", "header_length", base.DEC)
-	-- totol_length
-	local n_total_length = ProtoField.uint16("NewIP.total_length", "total_length", base.DEC)
+	-- Security 0x03 
+	local n_securty = ProtoField.string("NewIP.securty", "n_securty", base.ASCII)
+	-- Policy 0x04 
+	local n_policy = ProtoField.string("NewIP.policy", "n_policy", base.ASCII)
+	-- Time To Live 0x05
+	local n_TTL = ProtoField.string("NewIP.TTL", "n_TTL", base.ASCII)
+	-- Source DevID 0x06
+	local n_source_dev_id = ProtoField.string("NewIP.source_dev_id", "n_source_dev_id", base.ASCII)
+	-- Source DevID 0x07
+	local n_dest_dev_id = ProtoField.string("NewIP.dest_dev_id", "n_dest_dev_id", base.ASCII)
+	-- Identification 0x08
+	local n_identification = ProtoField.string("NewIP.identification", "n_identification", base.ASCII)
+	-- totol_length 0x09
+	local n_total_length = ProtoField.string("NewIP.total_length", "n_total_length", base.ASCII)
+	-- header_length 0x0a
+	local n_header_length = ProtoField.string("NewIP.header_length", "n_header_length", base.ASCII)
+	-- NextHeader 0x0b
+	local n_next_header = ProtoField.string("NewIP.next_header", "n_next_header", base.ASCII)
+	-- Source ServiceID 0x0c
+	local n_source_service_id = ProtoField.string("NewIP.source_service_id", "n_source_service_id", base.ASCII)
+	-- Destination ServiceID 0x0d
+	local n_dest_service_id = ProtoField.string("NewIP.dest_service_id", "n_dest_service_id", base.ASCII)
+	-- Fragment Offset 0x0e
+	local n_fragment_offset= ProtoField.string("NewIP.fragment_offset", "n_fragment_offset", base.ASCII)
 	--data
 	local n_data = ProtoField.bytes("NewIP.ndata", "n_data", base.SPACE)
 
-
     --这里把NewIP协议的全部字段都加到p_NewIP这个变量的fields字段里
-	p_NewIP.fields = {n_dest_address_TL, n_dest_address,n_source_address_TL, n_source_address, n_TTL, n_protocol,n_header_length,n_total_length,n_data}
+	p_NewIP.fields = {n_dest_address_TL, n_dest_address,n_source_address_TL,n_source_address, n_securty,n_policy,
+	n_TTL,n_source_dev_id,n_dest_dev_id,n_identification,n_total_length,n_header_length,n_next_header,
+	n_source_service_id ,n_dest_service_id,n_fragment_offset,n_data}
     --这里是获取data这个解析器
-    local data_dis = Dissector.get("data")
-    
-    local function ipn_dissector(buf,pkt,root)
-		local buflen = buf:len()
-		local offset = 0;
-		--添加Packet Details
-        local t = root:add(p_NewIP,buf)
-        --在Packet List窗格的Protocol列可以展示出协议的名称
-        pkt.cols.protocol = "NewIP"
+	local data_dis = Dissector.get("data")
 	
+	-- 树状结构
+	local t = ""
+	--  偏移量
+	local offset = 0
+	-- buf的长度
+	local buflen = 0
+	-- header_length
+	local header_length_value=-1
+	-- Next Header
+	local next_header_value=0
 
-		-- 目标地址
-		-- 目标地址标识符及长度
-		local v_dest_address_TL = buf(offset, 2)
-		local temp_dest_address_TL = buf(offset, 2):uint()
-		local dest_address_type = buf(offset,1):uint()
-		offset = offset + 1
-        local dest_address_length = buf(offset, 1):uint()
-        local dest_address_TL_to_show = "Destination Address Type: "..string.format("%#x",dest_address_type).."  Destination Address Length: "..dest_address_length
-		t:add(n_dest_address_TL, v_dest_address_TL, temp_dest_address_TL, dest_address_TL_to_show)
-		offset = offset + 1
-		-- 目标地址及层级
-		local v_dest_address = buf(offset, dest_address_length)
-		local temp_dest_address = buf(offset, dest_address_length):string()
-		local dest_address_toshow = "Destination Address: "..showAddress(buf,offset,dest_address_length ).."  Level:"..showAddressLevel(buf,offset, dest_address_length )
-		-- local dest_address_toshow = "Destination Address: "..showAddress(buf,offset,dest_length)
-		t:add(n_dest_address, v_dest_address, temp_dest_address, dest_address_toshow)
-		offset = offset + dest_address_length 
+	--  填充一般的采用T-L-V的字段
+	local function fill_fields(n_field,field_name,buf)
+		local field_length = buf(offset+1,1):uint()
+		local v_field = buf(offset,2+field_length)
+		local tmp_field = buf(offset,2+field_length):string()
+		offset = offset + 2
+		local field_value = buf(offset,field_length):uint()
+		local field_toshow = field_name..string.format("%d",field_value)
+		t:add(n_field,v_field,tmp_field,field_toshow)
+		offset = offset + field_length
+	end
 
 
-		-- 源地址
+	-- 各字段配置函数
+	local switch = {
+		[1] = function(buf)    -- 源地址
 		-- 源地址标识符及长度
 		local v_source_address_TL = buf(offset, 2)
 		local temp_source_address_TL = buf(offset, 2):uint()
@@ -161,73 +178,138 @@ do
 		local v_source_address = buf(offset, source_address_length)
 		local temp_source_address = buf(offset, source_address_length):string()
 		local source_address_toshow = "Source Address: "..showAddress(buf,offset,source_address_length).."  Level:"..showAddressLevel(buf,offset, source_address_length)
-		-- local source_address_toshow = "Source Address: "..showAddress(buf,offset,source_length)
-
 		t:add(n_source_address, v_source_address, temp_source_address, source_address_toshow)
 		offset = offset + source_address_length
 
-		-- TTL
-		local v_TTL = buf(offset,3)
-		local temp_TTL = buf(offset,3):uint()
-		offset = offset + 2
-		local TTL_value = buf(offset,1):uint()
-		local TTL_toshow = "Time To Live: "..string.format("%d",TTL_value)
-		t:add(n_TTL,v_TTL,temp_TTL,TTL_toshow)
-		offset = offset + 1
-	
-		-- next_header
-		local v_protocol = buf(offset, 3)
-		local temp_protocol = buf(offset, 3):uint()
-		offset = offset + 2
-		local protocol_value = buf(offset, 1):uint()
-		-- print(temp_protocol)
-		offset = offset + 1
+		end,
+		[2] = function(buf)    -- 目标地址
+			-- 目标地址标识符及长度
+			local v_dest_address_TL = buf(offset, 2)
+			local temp_dest_address_TL = buf(offset, 2):uint()
+			local dest_address_type = buf(offset,1):uint()
+			offset = offset + 1
+        	local dest_address_length = buf(offset, 1):uint()
+        	local dest_address_TL_to_show = "Destination Address Type: "..string.format("%#x",dest_address_type).."  Destination Address Length: "..dest_address_length
+			t:add(n_dest_address_TL, v_dest_address_TL, temp_dest_address_TL, dest_address_TL_to_show)
+			offset = offset + 1
+			-- 目标地址及层级
+			local  v_dest_address = buf(offset, dest_address_length)
+			local temp_dest_address = buf(offset, dest_address_length):string()
+			local dest_address_toshow = "Destination Address: "..showAddress(buf,offset,dest_address_length ).."  Level:"..showAddressLevel(buf,offset, dest_address_length )
+			t:add(n_dest_address, v_dest_address, temp_dest_address, dest_address_toshow)
+			offset = offset + dest_address_length 
+		end,
+		[3] = function(buf)    -- for Security
+			fill_fields(n_TTL,"Security: ",buf)
+		end,
+		[5] = function(buf)    -- for Time To Live
+			fill_fields(n_TTL,"Time To Live: ",buf)
+		end,
+		[9] = function(buf)    -- for Total Length
+			fill_fields(n_total_length,"Total Length: ",buf)
+		end,
+		[10] = function(buf)    -- for Header Length
+			local field_length = buf(offset+1,1):uint()
+			header_length_value = buf(offset+2,field_length):uint()
+			fill_fields(n_header_length,"Header Length: ",buf)
+		end,
+		[11] = function(buf)    -- for Next Header
+			local field_name=""
+			next_header_value=buf(offset+2,1):uint()
+			if  next_header_value== 58 then
+				field_name="Next Header: (ICMPv6)"
+			elseif  next_header_value == 6 then 
+				field_name="Next Header: (TCP)"
+			elseif  next_header_value == 17 then
+				field_name="Next Header: (UDP)"
+			else 
+				field_name="Next Header: (Other)"
+			end
+			fill_fields(n_next_header,field_name,buf)
+		end
+	}
 
-		-- HeaderLength
-		local v_header_length = buf(offset,3)
-		temp_header_length = buf(offset,3):uint()
-		offset = offset + 2
-		local header_length_value = buf(offset,1):uint()
-		local header_length_toshow = "Header Length: "..string.format("%d",header_length_value )
+    local function ipn_dissector(buf,pkt,root)
+		buflen = buf:len()
+		--添加Packet Details
+		t = root:add(p_NewIP,buf)
+
+		offset = 0
+		-- header_length
+		header_length_value=-1
+		-- Next Header
+		next_header_value=0
+        --在Packet List窗格的Protocol列可以展示出协议的名称
+        pkt.cols.protocol = "NewIP"
 		
-		offset = offset + 1
+		-- 字段的个数,循环保险措施
+		local field_num=0
+		-- 对齐偏移量
+		local padding_offset=0
+		-- 使用while循环进行代码的
+		while(header_length_value~=padding_offset and field_num<30) 
+		-- while(header_length_value ~= padding_offset) 
+		do 
+			local proto_field_type=buf(offset, 1):uint()
+			local match_field=switch[proto_field_type]
+			if(match_field) then
+				match_field(buf)
+			end
+			field_num=field_num+1
+			padding_offset=4*(offset+3)/4
+		end
 
-		-- TotalLength
-		-- 先确定TotalLength值的长度
-		local totol_length_length=buf(offset+1,1):uint()
-		local v_total_length = buf(offset,2+totol_length_length)
-		local temp_total_length = buf(offset,2+totol_length_length):uint()
-		offset = offset + 2
-		local total_length_value = buf(offset,totol_length_length):uint()
-		local total_length_toshow = "Total Length: "..string.format("%d",total_length_value)
-		offset = offset + totol_length_length
+		-- -- TTL
+		-- local v_TTL = buf(offset,3)
+		-- local temp_TTL = buf(offset,3):uint()
+		-- offset = offset + 2
+		-- local TTL_value = buf(offset,1):uint()
+		-- local TTL_toshow = "Time To Live: "..string.format("%d",TTL_value)
+		-- t:add(n_TTL,v_TTL,temp_TTL,TTL_toshow)
+		-- offset = offset + 1
+	
+		-- -- next_header
+		-- local v_protocol = buf(offset, 3)
+		-- local temp_protocol = buf(offset, 3):uint()
+		-- offset = offset + 2
+		-- local next_header_value = buf(offset, 1):uint()
+		-- -- print(temp_protocol)
+		-- offset = offset + 1
 
-		-- 直接用头部长度来作偏移量
+		-- -- HeaderLength
+		-- local v_header_length = buf(offset,3)
+		-- temp_header_length = buf(offset,3):uint()
+		-- offset = offset + 2
+		-- local header_length_value = buf(offset,1):uint()
+		-- local header_length_toshow = "Header Length: "..string.format("%d",header_length_value )
+		
+		-- offset = offset + 1
+
+		-- -- TotalLength
+		-- -- 先确定TotalLength值的长度
+		-- local totol_length_length=buf(offset+1,1):uint()
+		-- local v_total_length = buf(offset,2+totol_length_length)
+		-- local temp_total_length = buf(offset,2+totol_length_length):uint()
+		-- offset = offset + 2
+		-- local total_length_value = buf(offset,totol_length_length):uint()
+		-- local total_length_toshow = "Total Length: "..string.format("%d",total_length_value)
+		-- offset = offset + totol_length_length
+
+		-- -- 直接用头部长度来作偏移量
 		offset = header_length_value 
 		-- data
 		local v_data = buf(offset, (buflen - offset))
-		local pro_toshow = "Next Header: "..protocol_value
-		if  protocol_value== 58 then
-			Dissector.get("icmpv6"):call(v_data:tvb(), pkt, root)
-			pro_toshow = pro_toshow.."(ICMPv6)"
-		end
-		if  protocol_value == 6 then 
-			Dissector.get("tcp"):call(v_data:tvb(), pkt, root)
-			pro_toshow = pro_toshow.."(TCP)"
-		end
-		if  protocol_value == 17 then
-			Dissector.get("udp"):call(v_data:tvb(), pkt, root)
-			pro_toshow = pro_toshow.."(UDP)"
-		end
 
-		if protocol_value ~= 58 and protocol_value ~= 6 and protocol_value ~= 17 then
+		if(next_header_value == 58) then
+			Dissector.get("icmpv6"):call(v_data:tvb(), pkt, root)
+		elseif(next_header_value == 6) then 
+			Dissector.get("tcp"):call(v_data:tvb(), pkt, root)
+		elseif(next_header_value == 17) then
+			Dissector.get("udp"):call(v_data:tvb(), pkt, root)
+		else
 			t:add(n_data, v_data)
 		end
-		t:add(n_protocol,v_protocol,temp_protocol,pro_toshow)
-		t:add(n_header_length,v_header_length,temp_header_length,header_length_toshow)
-		t:add(n_total_length,v_total_length,temp_total_length,total_length_toshow)
 		pkt.cols.protocol = "NewIP"
-		
 		return true
     end
     
